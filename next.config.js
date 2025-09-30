b80a35689bffbc235355f4cc68ef920729b368a7
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2025-10-01 00:54:08 +0200

    fix: remove experimental tracing config for Vercel

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
+  distDir: '.next', // no uses variable de entorno para distDir
   eslint: {
     ignoreDuringBuilds: true,
   },
